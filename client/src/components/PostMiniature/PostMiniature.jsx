4f0cf85f9576a7a5ac7d789bd9251f430ded847f
leo-alvarenga <leosgalvarenga@protonmail.com>
2021-09-02 17:17:16 -0300

    Initial commit on frontend

---
+++ client/src/components/PostMiniature/PostMiniature.jsx
@@ -0,0 +1,10 @@
+import localStyles from "./PostMiniature.style";
+
+const PostMiniature = (props) => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default PostMiniature;
---
+++ client/src/pages/404/404.style.jsx
@@ -0,0 +1,7 @@
+import { makeStyles } from "@material-ui/styles";
+
+const localStyles = makeStyles(() => ({
+
+}));
+
+export default localStyles;
